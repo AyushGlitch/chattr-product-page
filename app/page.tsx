0c49d9f48363e7027a4b0e8bba1d52d965b65fe2
Ayush Aryan Singh <129523895+AyushGlitch@users.noreply.github.com>
2024-10-06 12:53:35 +0000

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<>
+			<div className="flex h-screen justify-center items-center text-3xl font-semibold">
+				Chattr: Scalable Chat App Product Page
+			</div>
+		</>	
+	);
+}
